627563c8f187a7747e772a934940c7353d5866e8
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-08 21:48:24 -0700

    working db create and fill with scrape

--- server.js
+++ server.js
@@ -32,11 +32,12 @@ app.set("view engine", "handlebars");
 app.set("index", __dirname + "/views");
 
 // If deployed, use the deployed database. Otherwise use the local  database
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/~~~DBNAME HERE";
-// then change the ine below to mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/CurrentConverse", { useNewUrlParser: true });
-var results = [];
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/CurrentConverse";
+// then change the ine below to 
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// // Connect to the Mongo DB -- old connection
+// mongoose.connect("mongodb://localhost/CurrentConverse", { useNewUrlParser: true });
+//var results = [];
 
 
 // Routes
@@ -50,51 +51,50 @@ app.get("/", function (req, res) {
 });
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
 	// First, we grab the body of the html with axios
 	axios.get("https://www.politico.com/").then((response) => {
 		// Then, we load that into cheerio and save it to $ for a shorthand selector
-		var $ = cheerio.load(response.data);
+        var $ = cheerio.load(response.data);
+        var titlesArray = [];
 		//console.log(response.data);
 
 		//check name change for article and h2 look for something like media-item, media-item-summary media-item image
 		// (meta__details p class for authors) (media-item_summary div class tease fo a short summary)
-        // (media-item_summary h1 class headline a href for link)
-        // stop trying story-frag.format-m
+		// (media-item_summary h1 class headline a href for link)
+		// stop trying story-frag.format-m
 		$(".media-item__summary").each((i, element) => {
-           // console.log("beginning of new article~~~~~~~~ /n", $(".media-item__summary").children().html());
+			// console.log("beginning of new article~~~~~~~~ /n", $(".media-item__summary").children().html());
 			// Save an empty result object
 			var result = {};
-            //console.log("element", element);--stop testing this
-            //console.log(i);-- returns the  number legnth of the element array
-            
+			//console.log("element", element);--stop testing this
+			//console.log(i);-- returns the  number legnth of the element array
+
 			//console.log(result);
 			// Add the text and href of every link, 
 			// and save them as properties of the result object
-                        // result.title = $(this)
-                        // 	.children("a")
-                        // 	.text();
-                        // result.link = $(this)
-                        // 	.children("a")
-                        // 	.attr("href");
-                        result.title = $(element).find("h1 a").text();
-                        result.link = $(element).find("h1 a").attr("href");
-                        //result.lede = $(element).find(".story-text").children("p")[0].children[0].data;
-           console.log("title result",result.title); // blank
-            console.log("title link",result.link); // undefined
-  
-		// 	// Create a new Article using the `result` object built from scraping
-		// 	db.Article.create(result)
-		// 		.then(function(dbArticle) {
-		// 			// View the added result in the console
-		// 			console.log(dbArticle);
-		// 		})
-		// 		.catch(function(err) {
-		// 			// If an error occurred, log it
-		// 			console.log(err);
-		// 		});
-		 });
-  
+
+			result.title = $(element).find("h1 a").text();
+			result.link = $(element).find("h1 a").attr("href");
+			result.tease = $(element).find(".tease").text();
+
+			//console.log("title result",result.title);
+			//console.log("title link",result.link); 
+            console.log("title tease", result.tease); // gives short summary where applicable
+            
+            
+			// Create a new Article using the `result` object built from scraping
+			db.Article.create(result)
+				.then(function (dbArticle) {
+					// View the added result in the console
+					console.log(dbArticle);
+				})
+				.catch(function (err) {
+					// If an error occurred, log it
+					console.log(err);
+				});
+		});
+
 		// Send a message to the client
 		res.send("Scrape Complete");
 	});
